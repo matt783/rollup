c06d055f33bcc6f1493b12f4b3b42641ff53d3aa
Laia <laisolizq@gmail.com>
2019-10-25 09:56:14 +0200

    general fixes

--- rollup-cli/index.js
+++ rollup-cli/index.js
@@ -2,21 +2,14 @@ const Db = require('./src/db');
 const KeyContainer = require('./src/kc');
 const ethereumWallet = require('./src/ethereum-wallet');
 const wallet = require('./src/wallet');
-
-const send = require('./src/actions/offchain/send');
-const deposit = require('./src/actions/onchain');
-const depositontop = require('./src/actions/onchain/depositOnTop');
-const forcewithdraw = require('./src/actions/onchain/forceWithdraw');
-const withdraw = require('./src/actions/onchain/withdraw');
+const onchain = require('./src/actions/onchain/onchain');
+const offchain = require('./src/actions/offchain/offchain');
 
 module.exports = {
     Db,
     KeyContainer,
-    send,
-    deposit,
-    depositontop,
-    forcewithdraw,
-    withdraw,
     ethereumWallet,
     wallet,
+    onchain,
+    offchain,
 };
