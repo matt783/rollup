08206cc1aad9ea28fdf365be8845b1fd0e0329a7
Laia <laisolizq@gmail.com>
2019-11-29 17:43:49 +0100

    small fixes

--- simple-webapp/src/App.js
+++ simple-webapp/src/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 
-import InitView from "./views/init-view";
-import ActionView from "./views/action-view";
+import InitView from './views/init-view';
+import ActionView from './views/action-view';
 
 class App extends Component {
-  
-  render(){
+  render() {
     return (
-      <React.Fragment>
-        <Route exact path="/" render={() => 
-          <InitView />
-        }
-        />
-        <Route exact path="/actions" render={() =>
-          <ActionView />
-        }/>
-      </React.Fragment>
+      <>
+        <Route
+          exact
+          path="/"
+          render={() => <InitView />} />
+        <Route
+          exact
+          path="/actions"
+          render={() => <ActionView />} />
+      </>
     );
   }
 }
 
-export default (App);;
+export default (App);
