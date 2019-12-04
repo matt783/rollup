740c8af9cfddb0e9003012e924ab93c5d0164cea
Laia <laisolizq@gmail.com>
2019-12-04 10:44:58 +0100

    small fixes

--- run-operator-param.sh
+++ run-operator-param.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#./run-operator-param.sh 9000 9001 http://127.0.0.1:10001
+#./run-operator-param.sh 9000 9001 http://127.0.0.1:10001 light/full/archive
 
 path_synch="$(pwd)/rollup-operator/test/config/synch-config-test.json"
 path_pool="$(pwd)/rollup-operator/test/config/pool-config-test.json"
@@ -9,7 +9,9 @@ echo "CONFIG_SYNCH = $path_synch
 CONFIG_POOL = $path_pool
 OPERATOR_PORT_ADMIN = $1
 OPERATOR_PORT_EXTERNAL = $2
-URL_SERVER_PROOF = $3" > rollup-operator/src/server/config.env
+URL_SERVER_PROOF = $3
+LOG_LEVEL = info
+OPERATOR_MODE = $4" > rollup-operator/src/server/config.env
 
 gnome-terminal -e 'node rollup-operator/src/server-proof.js'
 
