1e85f015a706d740660429bda9a831d3f387196a
Jesus <jesus.ligero@yahoo.es>
2019-12-11 12:38:51 +0100

    try deployment

--- run-operator.sh
+++ run-operator.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-gnome-terminal -e 'node rollup-operator/src/server-proof.js'
-
-gnome-terminal -e 'node rollup-operator/src/server/operator.js'
-
-gnome-terminal -e "node rollup-operator/src/server/register-op.js"
-
-exit
+node rollup-operator/src/server-proof.js &
+sleep 5
+node rollup-operator/src/server/operator.js
