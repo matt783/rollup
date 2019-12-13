a5e0ba3c164d5ae1bce4f2f1a512f54209931b1a
Laia <laisolizq@gmail.com>
2019-12-13 15:39:18 +0100

    organizational changes

--- copy-config.sh
+++ copy-config.sh
@@ -2,5 +2,5 @@
 
 mkdir -p docker/docker-webapp/config-webapp
 mkdir -p docker/runOperator/config-operator
-cp -R docker/config/* docker/docker-webapp/config-webapp
-cp -R docker/config/* docker/runOperator/config-operator
+cp -R docker/deployContracts/config-deploy/*.json docker/docker-webapp/config-webapp
+cp -R docker/deployContracts/config-deploy/*.json docker/runOperator/config-operator
