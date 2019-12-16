e79ca98e8d19747b3c342298deb6b8ce3a79dd43
Laia <laisolizq@gmail.com>
2019-12-16 14:31:47 +0100

    rm tmp operators

--- copy-config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p docker/docker-webapp/config-webapp
-mkdir -p docker/runOperator/config-operator
-cp -R docker/deployContracts/config-deploy/*.json docker/docker-webapp/config-webapp
-cp -R docker/deployContracts/config-deploy/*.json docker/runOperator/config-operator
